12a72628027c747748f0d3d724ee6c83e5de4e76
akshayb407 <akshayb407@gmail.com>
2022-09-26 17:26:39 +0530

    All Problems added

---
+++ CoinToss.js
@@ -0,0 +1,9 @@
+const HEAD = 0;
+const TAIL = 1;
+
+let random = Math.floor((Math.random()*10 )%2);
+if(random==HEAD){
+    console.log("The OutPut is Head's!!")
+}else{
+    console.log("The OutPut is Tail's!!");
+}
